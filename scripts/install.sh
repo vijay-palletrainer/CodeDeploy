9aa5d0c3c639ed1cfe8504c2c2df9ed322bde82f
vijay-palletrainer <palletrainer12@techpalle.com>
2025-04-16 18:19:06 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-
-# Set working directory to deployment directory
-DEPLOY_DIR="/home/ec2-user/flask-app"
-
-cd $DEPLOY_DIR
-
-# Make sure we own the directory
-sudo chown -R ec2-user:ec2-user $DEPLOY_DIR
-
-# Create virtual environment
+sudo cd /home/ec2-user/flask-app
 sudo python3 -m venv venv
-
-# Activate virtual environment
-source venv/bin/activate
-
-# Install requirements
+sudo source venv/bin/activate
 sudo pip install -r requirements.txt
