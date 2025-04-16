add4cb4c4f2f8bd41063238be25d95ff200bfee9
vijay-palletrainer <palletrainer12@techpalle.com>
2025-04-16 18:20:47 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sudo cd /home/ec2-user/flask-app
-sudo python3 -m venv venv
-sudo source venv/bin/activate
-sudo pip install -r requirements.txt
+cd /home/ec2-user/flask-app
+
+# Install pip if not already installed
+sudo yum install -y python3-pip
+
+# Set up virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
